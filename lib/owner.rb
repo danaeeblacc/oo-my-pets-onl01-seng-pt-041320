4e4bb8736b0868bb16e0c1bd09dff58007f1589f
Teinelle Charles <teinelledcharles@gmail.com>
2020-05-22 05:09:21 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -50,15 +50,15 @@ class Owner
     end
   end
 
-  # def feed_fish
-  #   @pets.collect do |species, instances|
-  #     if species == :fishes
-  #       instances.each do |fish|
-  #         fish.mood = "happy"
-  #       end
-  #     end
-  #   end
-  # end
+  def feed_fish
+    @pets.collect do |species, instances|
+      if species == :fishes
+        instances.each do |fish|
+          fish.mood = "happy"
+        end
+      end
+    end
+  end
 
   def sell_pets
     @pets.collect do |species, instances|
